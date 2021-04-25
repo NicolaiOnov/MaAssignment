a23ae54622e2d413fdaddc2cdbdf12ca189460fa
githuB624228G <kensaii999@gmail.com>
2021-04-25 16:13:42 +0200

    created a boilerplate spring app

---
+++ AccountsApi/src/test/java/ma/noncom/AccountsApi/AccountsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.noncom.AccountsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
