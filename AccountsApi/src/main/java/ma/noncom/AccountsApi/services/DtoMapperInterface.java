a70e1092144a60e6ed105b60a4279a3a3dc7717c
githuB624228G <kensaii999@gmail.com>
2021-04-27 16:57:17 +0200

    removed @service from interfaces

--- AccountsApi/src/main/java/ma/noncom/AccountsApi/services/DtoMapperInterface.java
+++ AccountsApi/src/main/java/ma/noncom/AccountsApi/services/DtoMapperInterface.java
@@ -4,7 +4,6 @@ import ma.noncom.AccountsApi.models.dto.AccountDto;
 import ma.noncom.AccountsApi.models.entity.Account;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface DtoMapperInterface {
     AccountDto mapToDto(Account account);
 }
