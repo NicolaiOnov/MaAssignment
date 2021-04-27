1f139b8f17feffa252c8aa81dfd0bdf1ac06464d
githuB624228G <kensaii999@gmail.com>
2021-04-27 17:28:34 +0200

    added logback config file with rollingFile logging enabled; now catching arithmetic error in dtoMapper and logging it

--- AccountsApi/src/main/java/ma/noncom/AccountsApi/services/DtoMapper.java
+++ AccountsApi/src/main/java/ma/noncom/AccountsApi/services/DtoMapper.java
@@ -1,5 +1,7 @@
 package ma.noncom.AccountsApi.services;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import ma.noncom.AccountsApi.models.dto.AccountDto;
 import ma.noncom.AccountsApi.models.entity.Account;
 import org.springframework.stereotype.Service;
@@ -9,11 +11,19 @@ import java.math.RoundingMode;
 
 @Service
 public class DtoMapper {
+    private final Logger logger = LoggerFactory.getLogger(DtoMapper.class);
+
     public AccountDto mapToDto(Account account) {
-        return new AccountDto(
-                account.getNumber(),
-                account.getBalanceInCents().divide(new BigDecimal("100"), 2, RoundingMode.DOWN),
-                account.getCurrency(),
-                account.getLastOperation());
+        try {
+            return new AccountDto(
+                    account.getNumber(),
+                    account.getBalanceInCents().divide(new BigDecimal("100"), 2, RoundingMode.DOWN),
+                    account.getCurrency(),
+                    account.getLastOperation());
+        }
+        catch (ArithmeticException ex) {
+            logger.error("A arithmetic error occurred during an attempt to convert balanceInCents to balance!", ex);
+            throw ex;
+        }
     }
 }
